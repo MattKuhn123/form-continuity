685bf971a0e2f8731edc0c46d19b70800889b18c
MattKuhn123 <102084893+MattKuhn123@users.noreply.github.com>
2025-09-16 12:06:57 -0400

    Create index-app.js

---
+++ index-app.js
@@ -0,0 +1,43 @@
+function onFormSubmit() {
+        showFormSubmitResultsDialog();
+        return false; // Prevent actual form submission
+    }
+
+    function getFormSubmitResultDialog() {
+        return document.getElementById('form-submit-result');
+    }
+
+    function showFormSubmitResultsDialog() {
+        getFormSubmitResultDialog().innerHTML = `<header><h2>Submitted!</h2></header>`;
+        for (const [key, value] of new FormData(getForm()).entries()) {
+            getFormSubmitResultDialog().innerHTML += `${key}: ${value}<br>`;
+        }
+
+        getFormSubmitResultDialog().innerHTML += `<footer><button onclick="this.parentElement.parentElement.close()" class="full-width">Close</button></footer>`;
+        getFormSubmitResultDialog().showModal();
+    }
+
+    function getEmailFieldValue() {
+        return getEmailField() ? getEmailField().value : '';
+    }
+
+    function getEmailField() {
+        return getForm().elements.namedItem('email');
+    }
+
+    function getConfirmEmailField() {
+        return getForm().elements.namedItem('confirm-email');
+    }
+
+    function isEmailConfirmed() {
+        return getEmailField()
+            && getEmailField().checkValidity()
+            && getEmailField().value !== ''
+            && getEmailField().value === getConfirmEmailField().value;
+    }
+
+    window.isSaveEnabled = isEmailConfirmed;
+    window.autoSaveKey = getEmailFieldValue;
+    document.getElementById("email").addEventListener('input', function () {
+        document.dispatchEvent(new CustomEvent(window.refreshEventKey));
+    });
