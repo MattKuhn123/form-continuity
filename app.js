180751db2edabb06550e264ca4b399f71d57bebc
Matt Kuhn <mlkkuhn@live.com>
2025-09-26 11:57:56 -0400

    fix dispatch refreshEventKey

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 function onFormSubmit() {
-        showFormSubmitResultsDialog();
-        return false; // Prevent actual form submission
+    showFormSubmitResultsDialog();
+    return false; // Prevent actual form submission
+}
+
+function getFormSubmitResultDialog() {
+    return document.getElementById('form-submit-result');
+}
+
+function showFormSubmitResultsDialog() {
+    getFormSubmitResultDialog().innerHTML = `<header><h2>Submitted!</h2></header>`;
+    for (const [key, value] of new FormData(getForm()).entries()) {
+        getFormSubmitResultDialog().innerHTML += `${key}: ${value}<br>`;
     }
 
-    function getFormSubmitResultDialog() {
-        return document.getElementById('form-submit-result');
-    }
+    getFormSubmitResultDialog().innerHTML += `<footer><button onclick="this.parentElement.parentElement.close()" class="full-width">Close</button></footer>`;
+    getFormSubmitResultDialog().showModal();
+}
 
-    function showFormSubmitResultsDialog() {
-        getFormSubmitResultDialog().innerHTML = `<header><h2>Submitted!</h2></header>`;
-        for (const [key, value] of new FormData(getForm()).entries()) {
-            getFormSubmitResultDialog().innerHTML += `${key}: ${value}<br>`;
-        }
+function getEmailFieldValue() {
+    return getEmailField() ? getEmailField().value : '';
+}
 
-        getFormSubmitResultDialog().innerHTML += `<footer><button onclick="this.parentElement.parentElement.close()" class="full-width">Close</button></footer>`;
-        getFormSubmitResultDialog().showModal();
-    }
+function getEmailField() {
+    return getForm().elements.namedItem('email');
+}
 
-    function getEmailFieldValue() {
-        return getEmailField() ? getEmailField().value : '';
-    }
+function getConfirmEmailField() {
+    return getForm().elements.namedItem('confirm-email');
+}
 
-    function getEmailField() {
-        return getForm().elements.namedItem('email');
-    }
+function isEmailConfirmed() {
+    return getEmailField()
+        && getEmailField().checkValidity()
+        && getEmailField().value !== ''
+        && getEmailField().value === getConfirmEmailField().value;
+}
 
-    function getConfirmEmailField() {
-        return getForm().elements.namedItem('confirm-email');
-    }
-
-    function isEmailConfirmed() {
-        return getEmailField()
-            && getEmailField().checkValidity()
-            && getEmailField().value !== ''
-            && getEmailField().value === getConfirmEmailField().value;
-    }
-
-    window.isSaveEnabled = isEmailConfirmed;
-    window.autoSaveKey = getEmailFieldValue;
-    document.getElementById("email").addEventListener('input', function () {
-        document.dispatchEvent(new CustomEvent(window.refreshEventKey));
-    });
+window.isSaveEnabled = isEmailConfirmed;
+window.autoSaveKey = getEmailFieldValue;
+document.querySelectorAll("#email, #confirm-email").forEach(x => x.addEventListener('input', function () {
+    document.dispatchEvent(new CustomEvent(window.refreshEventKey));
+}));
